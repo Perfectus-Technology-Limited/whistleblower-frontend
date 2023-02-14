eccd08fb67c99021e3bcf51edbd1f850b8dbc4df
kiruththigan <kiru1997kiruthi@gmail.com>
2023-02-14 16:40:49 +0530

    button style changed for mobile navbar

--- src/components/web3/CustomConnectionButton.js
+++ src/components/web3/CustomConnectionButton.js
@@ -1,5 +1,6 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useEffect, useState } from "react";
+
 export const CustomConnectionButton = () => {
   const [windowSize, setWindowSize] = useState(0);
   const [balance, setBalance] = useState("");
@@ -38,10 +39,8 @@ export const CustomConnectionButton = () => {
           (!authenticationStatus || authenticationStatus === "authenticated");
 
         useEffect(() => {
-          if (account.displayBalance) {
+          if (account) {
             setBalance(account.displayBalance);
-          }
-          if (account.displayName) {
             setAddress(account.displayName);
           }
         }, [account]);
@@ -125,6 +124,155 @@ export const CustomConnectionButton = () => {
                     {windowSize > 1100
                       ? address + "(" + balance + ")"
                       : balance}
+                  </button>
+                </div>
+              );
+            })()}
+          </div>
+        );
+      }}
+    </ConnectButton.Custom>
+  );
+};
+
+// for small scrren
+export const CustomConnectionButtonForMobile = () => {
+  const [windowSize, setWindowSize] = useState(0);
+  const [balance, setBalance] = useState("");
+  const [address, setAddress] = useState("");
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  });
+
+  return (
+    <ConnectButton.Custom>
+      {({
+        account,
+        chain,
+        openAccountModal,
+        openChainModal,
+        openConnectModal,
+        authenticationStatus,
+        mounted,
+      }) => {
+        // Note: If your app doesn't use authentication, you
+        // can remove all 'authenticationStatus' checks
+        const ready = mounted && authenticationStatus !== "loading";
+        const connected =
+          ready &&
+          account &&
+          chain &&
+          (!authenticationStatus || authenticationStatus === "authenticated");
+
+        useEffect(() => {
+          if (account) {
+            setBalance(account.displayBalance);
+            setAddress(account.displayName);
+          }
+        }, [account]);
+
+        return (
+          <div
+            {...(!ready && {
+              "aria-hidden": true,
+              style: {
+                opacity: 0,
+                pointerEvents: "none",
+                userSelect: "none",
+              },
+            })}
+          >
+            {(() => {
+              if (!connected) {
+                return (
+                  <button
+                    onClick={openConnectModal}
+                    type="button"
+                    className="connect-btn"
+                    style={{
+                      width: "100%",
+                    }}
+                  >
+                    Connect
+                  </button>
+                );
+              }
+
+              if (chain.unsupported) {
+                return (
+                  <button
+                    onClick={openChainModal}
+                    type="button"
+                    style={{ color: "red" }}
+                    className="connect-btn"
+                  >
+                    Wrong network
+                  </button>
+                );
+              }
+
+              return (
+                <div>
+                  <button
+                    onClick={openChainModal}
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      width: "100%",
+                      margin: "4px",
+                      marginTop: "0px",
+                    }}
+                    type="button"
+                    className="connect-btn"
+                  >
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        marginLeft: "auto",
+                        marginRight: "auto",
+                      }}
+                    >
+                      {chain.hasIcon && (
+                        <div
+                          style={{
+                            background: chain.iconBackground,
+                            width: 25,
+                            height: 25,
+                            borderRadius: 999,
+                            overflow: "hidden",
+                            marginRight: 4,
+                          }}
+                        >
+                          {chain.iconUrl && (
+                            <img
+                              alt={chain.name ?? "Chain icon"}
+                              src={chain.iconUrl}
+                              style={{ width: 25, height: 25 }}
+                            />
+                          )}
+                        </div>
+                      )}
+
+                      {chain.name}
+                    </div>
+                  </button>
+                  <button
+                    onClick={openAccountModal}
+                    type="button"
+                    className="connect-btn"
+                    style={{ width: "100%", margin: "4px" }}
+                  >
+                    {address + "(" + balance + ")"}
                   </button>
                 </div>
               );
