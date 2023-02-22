3efbddc149fc0186e6c6a15a5f40d227bd1c4f66
kiruththigan <kiru1997kiruthi@gmail.com>
2023-02-22 19:59:39 +0530

    map code refactor

--- src/components/Home/Map/Map.js
+++ src/components/Home/Map/Map.js
@@ -1,12 +1,16 @@
 import React from "react";
 import * as d3 from "d3";
 import Marker from "./Markers";
+import Paths from "./Paths";
 
 export default function Map() {
   const projection = d3.geoMercator();
   return (
     <>
-      <Marker projection={projection} />
+      <svg viewBox="0 0 960 500">
+        <Paths projection={projection} />
+        <Marker projection={projection} />
+      </svg>
     </>
   );
 }
