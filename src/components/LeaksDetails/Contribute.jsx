2c5114b0c9bc6ac4a9d6efc2defa504d1c3c2bde
sarubi <stsarut@gmail.com>
2023-03-02 17:53:40 +0530

    comments section added

---
+++ src/components/LeaksDetails/Contribute.jsx
@@ -0,0 +1,131 @@
+import React, { useEffect, useState } from "react";
+import { LikeFilled, DislikeFilled } from "@ant-design/icons";
+import { useContractRead, useContractWrite } from "wagmi";
+import { whistleblowerConfig } from "@/blockchain/bsc/web3.config";
+import { Button, Input, message, Modal, Spin } from "antd";
+import { prepareWriteContract, writeContract } from "@wagmi/core";
+const {TextArea} =Input;
+
+const styles = {
+  contributeContainer: {
+    margin: "40px 0",
+  },
+  contribute: {
+    color: "#ffffff",
+    fontSize: "18px",
+    fontWeight: "800",
+  },
+  signHere: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-start",
+  },
+  signHereBtn: {
+    justifyContent: "center",
+    display: "flex",
+    width: "150px",
+    color: "#00A771",
+    fontWeight: "600px",
+    fontSize: "20px",
+    alignItems: "center",
+    border: "1px solid #00A771",
+    borderRadius: "5px",
+    padding: "5px 10px",
+    marginTop: "30px",
+    marginRight: "20px",
+    background: "none",
+  },
+};
+
+function Contribute({ leakCID }) {
+  const [commentLoading, setCommentLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [comment, setComment] = useState("");
+
+  const showModal = () => {
+    setOpen(true);
+  };
+  
+  const handleOk = async () => {
+    try {
+      setCommentLoading(true);
+      const config = await prepareWriteContract({
+        address: whistleblowerConfig?.contractAddress,
+        abi: whistleblowerConfig?.contractAbi,
+        functionName: "comment",
+        args: [leakCID, comment],
+      });
+      const txReceipt = await writeContract(config);
+      await txReceipt.wait();
+      setCommentLoading(false);
+      message.success("Your comment has been recorded");
+    } catch (error) {
+      let errorMessage = "Something went wrong while trying to your comment";
+      if (error && error.message) {
+        errorMessage = error.message;
+      }
+      if (error && error.reason && error.reason !== "") {
+        errorMessage = error.reason;
+      }
+      message.error(errorMessage);
+      console.log("ERROR while trying to comment", error);
+      setCommentLoading(false);
+    }
+  };
+  
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className="vote-cast-container" style={styles.contributeContainer}>
+      <div className="vote-cast-title" style={styles.contribute}>
+        Contribute :
+      </div>
+
+      <div className="sign-here" style={styles.signHere}>
+        <div className="comment-count"></div>
+
+        <button
+          style={styles.signHereBtn}
+          onClick={showModal}
+        >
+          Sign Here
+        </button>
+
+        <Modal
+         maskStyle={{ background: "#101420", opacity: "0.9" }}
+          onCancel={handleCancel}
+          open={open}
+          title="Comment Here"
+          footer={[
+            <Button
+              key="submit"
+              type="primary"
+              loading={commentLoading}
+              onClick={handleOk}
+            >
+              Submit
+            </Button>,
+          ]}
+        >
+          <div style={{ marginBottom: "30px" }}>
+            <TextArea
+              showCount
+              maxLength={140}
+              style={{
+                height: 100,
+                resize: "none",
+              }}
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+              placeholder="Comment here ."
+            />
+          </div>
+        </Modal>
+      </div>
+    </div>
+  );
+}
+
+export default Contribute;
--- src/views/LeakDetailsPage.jsx
+++ src/views/LeakDetailsPage.jsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { fetchDataFromIPFSCID } from "@/services/ipfs.service";
-import { Col, Row, Spin } from "antd";
+import { Avatar, Card, Col, Row, Space, Spin } from "antd";
 import LeakHeader from "@/components/LeaksDetails/LeakHeader";
 import LeakDescription from "@/components/LeaksDetails/LeakDescription";
 import AuthorDetails from "@/components/LeaksDetails/AuthorDetails";
 import LeakFilesSection from "@/components/LeaksDetails/LeakFilesSection";
-import VoteCastWidget from "@/components/LeaksDetails/VoteCastWidget";
+// import VoteCastWidget from "@/components/LeaksDetails/VoteCastWidget";
+import Contribute from "@/components/LeaksDetails/Contribute";
+import { UserOutlined } from "@ant-design/icons";
+import { useContractRead } from "wagmi";
+import { whistleblowerConfig } from "@/blockchain/bsc/web3.config";
+import Jazzicon from "react-jazzicon/dist/Jazzicon";
+import { jsNumberForAddress } from "react-jazzicon";
+import { shortenEthAddress, truncateText } from "@/utils/helpers";
 
 const styles = {
   pageContainer: {
@@ -24,11 +31,40 @@ const styles = {
     color: "#ffffff",
   },
 };
+
 function LeakDetailsPage({ ipfsCID }) {
   const [isLeaksDataLoading, setIsLeaksDataLoading] = useState(false);
   const [leakData, setLeakData] = useState(null);
+  let commentsWithUser = [];
+
+  const { data: comments, isLoading: commentsLoading } = useContractRead({
+    address: whistleblowerConfig?.contractAddress,
+    abi: whistleblowerConfig?.contractAbi,
+    functionName: "getAllComments",
+    args: [ipfsCID],
+    // enabled: !!ipfsCID,
+    // watch: true,
+  });
+
+  const { data: commentedUsers, isLoading: commentedUserLoading } =
+    useContractRead({
+      address: whistleblowerConfig?.contractAddress,
+      abi: whistleblowerConfig?.contractAbi,
+      functionName: "getAllCommentedUsers",
+      args: [ipfsCID],
+      // enabled: !!ipfsCID,
+      // watch: true,
+    });
+
+  for (let i = 0; i < comments?.length; i++) {
+    commentsWithUser.push({
+      user: commentedUsers[0],
+      comment: comments[0],
+    });
+  }
 
   console.log(leakData);
+
   useEffect(() => {
     if (ipfsCID) {
       fetchDataFromIPFS();
@@ -70,13 +106,148 @@ function LeakDetailsPage({ ipfsCID }) {
                 date={leakData?.date}
               />
 
-              <VoteCastWidget leakCID={ipfsCID} />
+              <Contribute leakCID={ipfsCID} />
               <div className="voting-container"></div>
 
               <LeakFilesSection files={leakData?.uploadedFiles} />
             </Col>
           </>
         )}
+      </Row>
+      <Row
+        style={{ marginTop: "40px", display: "flex", flexDirection: "column" }}
+      >
+        <div style={{ color: "white", fontSize: "20px", fontWeight: "400" }}>
+          Comments
+        </div>
+        {commentsWithUser.map((comment) => (
+          <Col sx={24} md={24} lg={16}>
+            <Card
+              style={{
+                padding: "0 10px 20px 10px",
+                marginTop: "30px",
+                background: "none",
+                border: "1px solid #134c04",
+                color: "white",
+              }}
+              sx={24} md={24} lg={16}
+            >
+              <Space direction="vertical" size={16}>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "left",
+                    alignItems: "center",
+                  }}
+                >
+                  <Space size={16}>
+                    <span>
+                      <Avatar
+                        size={"large"}
+                        icon={<Jazzicon
+                          diameter={35}
+                          seed={jsNumberForAddress(
+                            comment.user ? comment.user.toString() : ""
+                          )}
+                        />}
+                        style={{ color: "white", background: "#888" }}
+                      />
+                    </span>
+                    <span >{shortenEthAddress(comment.user, 8)}</span>
+                  </Space>
+                </div>
+                <div>
+                  <span style={{ color: "#aaa" }}>{comment.comment}</span>
+                </div>
+              </Space>
+            </Card>
+          </Col>
+        ))}
+        {/* 
+        <Col sx={24} md={24} lg={16}>
+          <Card
+            style={{
+              padding: "0 10px 20px 10px",
+              marginTop: "30px",
+              background: "none",
+              border: "1px solid #134c04",
+              color: "white",
+            }}
+          >
+            <Space direction="vertical" size={16}>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "left",
+                  alignItems: "center",
+                }}
+              >
+                <Space size={16}>
+                  <span>
+                    <Avatar
+                      size={"large"}
+                      icon={<UserOutlined />}
+                      style={{ color: "white", background: "#888" }}
+                    />
+                  </span>
+                  <span>0x1dAe7a8F39F831320DC8B6e436dd687f421B8EF0</span>
+                </Space>
+              </div>
+              <div>
+                <span style={{ color: "#aaa" }}>
+                  sdf sdfsdfg sdfgsdfg sdfgsdfg sdrgsdfg sdfgsdfg sdfgsdfg
+                  sdfgsdfg sdfgsdfg safgsdfg sdfgsdfg sdfgsdfg sdfgsdfg sdfgsdfg
+                  sdfgsdfg sdfgsdfg
+                </span>
+              </div>
+            </Space>
+          </Card>
+        </Col>
+
+        <Col sx={24} md={24} lg={16}>
+          <Card
+            style={{
+              padding: "0 10px 20px 10px",
+              marginTop: "30px",
+              background: "none",
+              border: "1px solid #134c04",
+              color: "white",
+            }}
+          >
+            <Space direction="vertical" size={16}>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "left",
+                  alignItems: "center",
+                }}
+              >
+                <Space size={16}>
+                  <span>
+                    <Avatar
+                      size={"large"}
+                      icon={<UserOutlined />}
+                      style={{ color: "white", background: "#888" }}
+                    />
+                  </span>
+                  <span style={{ overflow: "hidden" }}>
+                    0x1dAe7a8F39F831320DC8B6e436dd687f421B8EF0
+                  </span>
+                </Space>
+              </div>
+              <div>
+                <span style={{ color: "#aaa" }}>
+                  sdf sdfsdfg sdfgsdfg sdfgsdfg sdrgsdfg sdfgsdfg sdfgsdfg
+                  sdfgsdfg sdfgsdfg safgsdfg sdfgsdfg sdfgsdfg sdfgsdfg sdfgsdfg
+                  sdfgsdfg sdfgsdfg
+                </span>
+              </div>
+            </Space>
+          </Card>
+        </Col> */}
       </Row>
     </div>
   );
