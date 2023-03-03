7e4256068aac832151d3e95592e66a05882346cb
Nipun Theekshana <theekshana2@gmail.com>
2023-03-03 17:53:02 +0530

    add websocket connection

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,7 +15,7 @@ import {
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { bscTestnet, bsc } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+import { useWebSocketProvider } from "wagmi";
 
 bsc.hasIcon = true;
 bsc.iconUrl = "/bscmain.png";
@@ -23,7 +23,31 @@ bscTestnet.name = "BSC Testnet";
 bscTestnet.hasIcon = true;
 bscTestnet.iconUrl = "/bsctest.png";
 
-const { chains, provider } = configureChains(
+export const BinanceTestnet = {
+  id: 97,
+  name: 'BSC Testnet',
+  network: 'bsc mainnet',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'Binance Chain Native Token',
+    symbol: 'tBNB',
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://data-seed-prebsc-2-s1.binance.org:8545'],
+      webSocket: ['wss://floral-late-shadow.bsc-testnet.discover.quiknode.pro/f646c1084c8ffd284d0538fbe89c622521e415bd/']
+    },
+  },
+  blockExplorers: {
+    default: { name: 'Bscscan', url: 'https://testnet.bscscan.com' },
+  },
+  hasIcon: true,
+  iconUrl: "https://umbria.network/cdn-cgi/mirage/3bbdf2483e57b9a6384adcb4992cf5d0494b4863df5f54b694cf999f87f739c6/1280/assets/images/icon/bsclogo.png?v1",
+  testnet: true,
+}
+
+
+const { chains, provider, webSocketProvider } = configureChains(
   [bscTestnet, bsc],
   [
     publicProvider(),
@@ -42,6 +66,7 @@ const wagmiClient = createClient({
   autoConnect: true,
   connectors,
   provider,
+  webSocketProvider
 });
 
 export default function App({ Component, pageProps }) {
